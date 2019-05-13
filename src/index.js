0fe86e45b33eb68edee7a0e35d58a5fa49021b06
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-13 11:39:32 -0400

    Added Redux Thunk to handle async fetch request

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import nycBreweries from './reducers/nycBreweries';
+import nycBreweriesReducer from './reducers/nycBreweriesReducer';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import 'semantic-ui-css/semantic.min.css';
 require('dotenv').config();
 
-const store = createStore(nycBreweries, applyMiddleware(thunk))
+const store = createStore(nycBreweriesReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
