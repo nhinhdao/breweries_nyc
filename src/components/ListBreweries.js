9bbe9b286bc74e72efd2065115f66f5f92d10e42
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-13 11:42:16 -0400

    Added component for rendering breweries list

--- src/components/ListBreweries.js
+++ src/components/ListBreweries.js
@@ -1,15 +1,43 @@
-import React, { Component } from 'react'
-import {List, Header, Segment, Icon} from 'semantic-ui-react';
+import React, {Component} from 'react';
+import times from 'lodash.times';
+import {List, Header, Segment, Icon, Menu} from 'semantic-ui-react';
 
 class ListBreweries extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      page: 0
+    }
+  }
+  setPage = (page) => {
+    return () => {
+      this.setState({page});
+    };
+  }
+
+  decrementPage = () => {
+    const {page} = this.state;
+    this.setState({page: page - 1});
+  }
+
+  incrementPage = () => {
+    const {page} = this.state;
+    this.setState({page: page + 1});
+  }
+
   render() {
+    const TOTAL_PER_PAGE = 9;
+    const {page} = this.state;
+    const {breweries, getBrewery} = this.props;
+    const startIndex = page * TOTAL_PER_PAGE;
+    const totalPages = Math.ceil(breweries.length / TOTAL_PER_PAGE);
     return (
       <Segment className='listPlaces'>
         <List divided animated verticalAlign='middle' id='listData'>
-          {this.props.breweries.map(place =>
+          {breweries.slice(startIndex, startIndex + TOTAL_PER_PAGE).map(place =>
             <List.Item key={place.id}>
-              <List.Content onClick={() => this.props.getBrewery(place.id)}>
+              <List.Content onClick={() => getBrewery(place.id)}>
                 <Header as='h4' color='blue'>{place.name}</Header>
                 <List.Description><Icon name='tag' /> {place.brewery_type}</List.Description>
                 <List.Description><Icon name='map marker alternate' /> {place.address}</List.Description>
@@ -18,6 +46,25 @@ class ListBreweries extends Component {
             </List.Item>
           )}
         </List>
+        { breweries.length > TOTAL_PER_PAGE &&
+          <Menu floated="right" pagination>
+            {page !== 0 &&
+              <Menu.Item as="a" icon onClick={this.decrementPage}>
+                <Icon name="left chevron" />
+              </Menu.Item>
+            }
+            {times(totalPages, n =>
+              (<Menu.Item as="a" key={n} active={n === page} onClick={this.setPage(n)}>
+                {n + 1}
+              </Menu.Item>),
+            )}
+            {page !== (totalPages - 1) &&
+              <Menu.Item as="a" icon onClick={this.incrementPage}>
+                <Icon name="right chevron" />
+              </Menu.Item>
+            }
+          </Menu>
+        }
       </Segment>
     )
   }
