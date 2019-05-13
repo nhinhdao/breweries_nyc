f53f48699a4adc650316b4bc2002f98f02cfc584
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-13 11:43:10 -0400

    Main CP for displaying map and list breweries

--- src/containers/GetBreweries.js
+++ src/containers/GetBreweries.js
@@ -1,72 +1,11 @@
 import React, {Component} from 'react';
-import {Icon, Input, Grid, Button} from 'semantic-ui-react';
+import {Icon, Input, Grid, Button, Message} from 'semantic-ui-react';
 import ListBreweries from '../components/ListBreweries';
 import Brewery from '../components/Brewery';
 import {connect} from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 import {getNYCBreweries, searchBreweriesByName, searchBreweriesByType} from '../actions/HandleAPIs';
-
-const list = [
-  {
-    id: 1,
-    name: "Adirondack Toboggan Company Microbrewery",
-    brewery_type: "micro",
-    address: "202A W Main St, Gouverneur, New York",
-    postal_code: "13642-1334",
-    country: "United States",
-    longitude: -75.4748923846074,
-    latitude: 44.3323731052224,
-    phone: "3157716313",
-    website_url: "http://www.adktoboggan.net"
-  },
-  {
-    id: 2,
-    name: "Black Forest Brew Haus",
-    brewery_type: "brewpub",
-    address: "2015 New Hwy, Farmingdale, New York",
-    postal_code: "11735-1103",
-    country: "United States",
-    longitude: -73.4144329120839,
-    latitude: 40.7550515,
-    phone: "6313919500",
-    website_url: "http://www.blackforestbrewhaus.com"
-  },
-  {
-    id: 3,
-    name: "Blue Point Brewing Co",
-    brewery_type: "large",
-    address: "161 River Ave, Patchogue, New York",
-    postal_code: "11772-3304",
-    country: "United States",
-    longitude: -73.0216062980388,
-    latitude: 40.75913445,
-    phone: "6314756944",
-    website_url: "http://www.bluepointbrewing.com"
-  },
-  {
-    id: 4,
-    name: "Brown's Brewing Co",
-    brewery_type: "brewpub",
-    address: "50 Factory Hill Rd, North Hoosick, New York",
-    postal_code: "12133",
-    country: "United States",
-    longitude: -73.3451582057618,
-    latitude: 42.9266144376744,
-    phone: "5182732337",
-    website_url: "http://www.brownsbrewing.com"
-  },
-  {
-    id: 5,
-    name: "Carey's Brew House",
-    brewery_type: "brewpub",
-    address: "58 Bridge St, Corning, New York",
-    postal_code: "14830-2239",
-    country: "United States",
-    longitude: -77.0608970416667,
-    latitude: 42.1502577083333,
-    phone: "6073775651",
-    website_url: "http://www.careysbrewhouse.com"
-  }
-]
+import {Marker} from '../components/RenderBrewery';
 
 class GetBreweries extends Component {
   constructor () {
@@ -74,10 +13,18 @@ class GetBreweries extends Component {
     this.state = {
       name: '',
       type: '',
+      breweries: [],
       brewery: null,
       byName: false,
       byType: false
     };
+    this.searchByName = this.searchByName.bind(this);
+    this.searchByType = this.searchByType.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.props.getNYCBreweries();
+    this.setState({breweries: this.props.nycBreweries})
   }
 
   handleChange = (event) => {
@@ -92,17 +39,28 @@ class GetBreweries extends Component {
     this.setState({byName: false, byType: true})
   }
 
-  searchByName = () => {
-    console.log(this.state.name);
+  async searchByName() {
+    await this.props.searchBreweriesByName(this.state.name);
+    this.setState({brewery: null, breweries: this.props.breweriesByName})
   }
 
-  searchByType = () => {
-    console.log(this.state.type);
+  async searchByType() {
+    await this.props.searchBreweriesByType(this.state.type);
+    this.setState({brewery: null, breweries: this.props.breweriesByType})
   }
 
   getBreweryOnClick = (id) => {
-    const place = list.find(place => place.id === id)
+    const place = this.state.breweries.find(place => place.id === id)
     this.setState({brewery: place})
+  }
+
+  getBreweries = () => {
+    this.setState({brewery: null, breweries: this.props.nycBreweries})
+  }
+
+  toggleBreweryCard = (id) => {
+    const place = this.state.breweries.find(place => place.id === id)
+    console.log(place)
   }
 
   render() {
@@ -112,6 +70,7 @@ class GetBreweries extends Component {
           <Grid.Row textAlign='center' className='searchPage' >
             <Grid.Column>
               <Button color='olive' onClick={this.getBreweries}>New York Breweries</Button>
+              <Button color='green' onClick={this.getMap}>View Map</Button>
               <Button color='blue' onClick={this.toggleSearchName}>Breweries by name</Button>
               <Button color='teal' onClick={this.toggleSearchType}>Breweries by type</Button>
               {this.state.byName &&
@@ -137,14 +96,37 @@ class GetBreweries extends Component {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row stretched>
-            <Grid.Column width={6}>
-              <ListBreweries breweries={list} getBrewery={this.getBreweryOnClick}/>
-            </Grid.Column>
+            {this.state.breweries.length > 0 ?
+              <Grid.Column width={6}>
+                <ListBreweries breweries={this.state.breweries} getBrewery={this.getBreweryOnClick} />
+              </Grid.Column> :
+              <Grid.Column width={16} textAlign='center'>
+                <Message negative>Brewery not found. Please try again!</Message>
+              </Grid.Column> 
+            }
             <Grid.Column width={10}>
               {this.state.brewery &&
                 <Brewery brewery={this.state.brewery} />
               }
             </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <div style={{height: '800px', width: '100%'}}>
+              <GoogleMapReact
+                bootstrapURLKeys={{key: `${process.env.REACT_APP_GG_API}`}}
+                defaultCenter={{lat: 42.165726, lng: -74.948051}}
+                defaultZoom={7}
+              >
+                {this.state.breweries.map(place => 
+                  <Marker
+                    key={place.id}
+                    lat={place.latitude}
+                    lng={place.longitude}
+                    brewery={place}
+                  />
+                )}
+              </GoogleMapReact>
+            </div> 
           </Grid.Row>
         </Grid>
       </div>
@@ -155,7 +137,7 @@ class GetBreweries extends Component {
 
 const mapStateToProps = state => {
   return {
-    nycBrewries: state.allBreweries,
+    nycBreweries: state.allBreweries,
     breweriesByName: state.breweriesByName,
     breweriesByType: state.breweriesByType
   }
