4a76d37a875b360a6dd7535e6e01b266c6084ca5
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-14 21:07:12 -0400

    Fix Microlink unmounted component error

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -8,10 +8,17 @@ import GoogleMapReact from 'google-map-react';
 const Marker = ({text}) => <Label size='tiny' color='blue' pointing>{text}</Label>
 
 export class Brewery extends Component {
+  _isMounted = false;
 
+  componentDidMount() {
+    this._isMounted = true;
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
   render() {
     const {brewery} = this.props
-    const center = {lat: 42.165726, lng: -74.948051} //New York State Long and Lat
     const text = brewery.name.split(" ").slice(0, 2).join(" ") //Short name to show on map marker
     return (
       <Segment>
@@ -29,10 +36,10 @@ export class Brewery extends Component {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <div style={{height: '450px', width: '100%'}}>
+            <div style={{height: '420px', width: '100%'}}>
               <GoogleMapReact
                 bootstrapURLKeys={{key: `${process.env.REACT_APP_GG_API}`}}
-                defaultCenter={center}
+                defaultCenter={{lat: 42.165726, lng: -74.948051}} //New York State Long and Lat
                 defaultZoom={6}
               >
                 <Marker
