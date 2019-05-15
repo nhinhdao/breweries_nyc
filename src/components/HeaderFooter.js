48851d69e453088ffeb3483ec63be3a4f7380b72
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-14 21:07:28 -0400

    Added Header and Footer components

---
+++ src/components/HeaderFooter.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from '../logo.svg';
+import {Segment, Container, Menu, Header, Button, Icon} from 'semantic-ui-react'
+
+function scrollDown() {
+  const yPos = document.getElementById('listBreweries').getBoundingClientRect().y;
+  window.scrollTo({
+    top: yPos,
+    left: 0,
+    behavior: 'smooth'
+  })
+}
+
+export function MyFooter() {
+  return (
+    <Segment vertical className='myFooter'>
+      <Container textAlign='center'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>&copy; {new Date().getFullYear()} Copyright: Nhinh Dao</p>
+      </Container>
+    </Segment>
+  )
+}
+
+export function MyHeader() {
+  return (
+    <header className='banner'>
+      <Menu>
+        <Container fluid>
+          <Menu.Item header>
+            <img src={logo} className="App-logo" alt="logo" />
+            NEW YORK BREWERIES
+          </Menu.Item>
+        </Container>
+      </Menu>
+      <Header as='h1' textAlign='center' icon className='logoHeader'>
+        <Icon circular inverted color='teal' name='beer' />
+        <Header className='content'>EXPLORE NEW YORK FINEST BREWING COMPANIES...</Header>
+        <Button color='olive' onClick={scrollDown}>Learn more</Button>
+      </Header>
+    </header>
+  )
+}
