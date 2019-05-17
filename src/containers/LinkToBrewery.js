d0209148959c8270d301cecc57386547d16dc06b
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-17 03:49:45 -0400

    Added component when user click on grid

--- src/containers/LinkToBrewery.js
+++ src/containers/LinkToBrewery.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {RenderBrewery} from '../components/RenderBrewery';
 import {searchBreweriesByID} from '../actions/HandleAPIs';
-import {Container, Segment, Button, Divider} from 'semantic-ui-react';
+import {Container, Button, Divider, Grid} from 'semantic-ui-react';
 
 class LinkToBrewery extends Component {
   state = {brewery: null};
@@ -13,13 +13,25 @@ class LinkToBrewery extends Component {
     await this.props.searchBreweriesByID(id);
     this.setState({brewery: this.props.brewery})
   }
+  
+  getBreweryIndex = () => {
+    const {breweries} = this.props;
+    const id = this.state.brewery.id;
+    return breweries.findIndex(brewery => brewery.id === id)
+  }
 
   handleNextBrewery = () => {
     const {breweries} = this.props;
-    const id = this.state.brewery.id;
-    const index = breweries.findIndex(brewery => brewery.id === id)
+    const index = this.getBreweryIndex();
     if (index < breweries.length - 1){
-      this.setState({brewery: breweries[index+1]})
+      this.setState({brewery: breweries[index + 1]});
+    }
+  }
+
+  handlePreviousBrewery = () => {
+    const index = this.getBreweryIndex();
+    if (index > 0) {
+      this.setState({brewery: this.props.breweries[index - 1]});
     }
   }
 
@@ -33,12 +45,25 @@ class LinkToBrewery extends Component {
     return (
       <Container fluid id='br-container'>
         <Container>
-          <RenderBrewery brewery={brewery} />
-          <Divider />
-          <Segment basic>
-            <Link to='/breweries'><Button color='teal'>Go Back</Button></Link>
-            <Button primary floated='right' onClick={this.handleNextBrewery}>Next Brewery</Button>
-          </Segment>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column widths={14}>
+                <RenderBrewery brewery={brewery} />
+              </Grid.Column>
+            </Grid.Row>
+            <Divider hidden />
+            <Grid.Row columns={3} textAlign='center'>
+              <Grid.Column>
+                <Button color='teal' onClick={this.handlePreviousBrewery} className='btn-individual'>Previous Brewery</Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Link to='/breweries'><Button color='orange' className='btn-individual'>Go Back</Button></Link>
+              </Grid.Column>
+              <Grid.Column>
+                <Button color='blue' onClick={this.handleNextBrewery} className='btn-individual'>Next Brewery</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
       </Container>
     )
