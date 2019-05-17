2737d5ef3243bae5b8431bb48bfeb1874bd911e8
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-17 03:48:43 -0400

    Added Separated component for popup Card on Map

--- src/components/RenderBrewery.js
+++ src/components/RenderBrewery.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import GoogleMapReact from 'google-map-react';
-import {Feed, Header, Icon, Popup, Card, Divider, Label, Grid} from 'semantic-ui-react';
+import {Feed, Header, Icon, Popup, Card, Label, Grid} from 'semantic-ui-react';
 
 export function RenderBrewery({brewery}) {
   return (
-    <Grid centered>
+    <Grid>
       <Header className='br-header' textAlign='center'>{brewery.name}</Header>
       <Grid.Row>
-        <Grid.Column width={9}>
+        <Grid.Column width={12}>
           <Feed className='br-des'>
             <Feed.Extra text><Icon name='tag' />{" "}{brewery.brewery_type}</Feed.Extra>
             <Feed.Extra text><Icon name='map marker alternate' />{" "}{brewery.address}</Feed.Extra>
             <Feed.Extra text><Icon name='globe' />{" "}{brewery.country}</Feed.Extra>
             <Feed.Extra text><Icon name='barcode' />{" "}{brewery.postal_code}</Feed.Extra>
             <Feed.Extra text><Icon name='phone' />{" "}{brewery.phone}</Feed.Extra>
-            <Feed.Extra text><Icon name='linkify' />{" "}<a href={`${brewery.website_url}`} rel="noopener noreferrer" target='_blank'>{brewery.website_url}</a></Feed.Extra>
+            <Feed.Extra text><Icon name='linkify' />{" "}
+              {(brewery.website_url === 'N/A') ? 'N/A' : <a href={`${brewery.website_url}`} rel="noopener noreferrer" target='_blank'>{brewery.website_url}</a>}
+            </Feed.Extra>
           </Feed>
         </Grid.Column>
       </Grid.Row>
@@ -30,23 +32,46 @@ export function RenderBrewery({brewery}) {
 
 export function RenderSummary({brewery}) {
   return (
-    <Card>
+    <Card className='card-individual'>
       <Card.Content>
-        <Link to={`/breweries/${brewery.id}`}><Header as='h4' color='blue' textAlign='center'>{brewery.name}</Header></Link>
-        <Divider />
-        <div style={{marginLeft: '20px'}}>
-          <Card.Description>
-            <Icon name='tag' />{" "}{brewery.brewery_type}
-          </Card.Description>
-          <Card.Description>
-            <Icon name='map marker alternate' />{" "}{brewery.address}
-          </Card.Description>
-          <Card.Description>
-            <Icon name='linkify' />{" "}<a href={`${brewery.website_url}`} rel="noopener noreferrer" target='_blank'>{brewery.website_url}</a>
-          </Card.Description>
-        </div>
+        <Link to={`/breweries/${brewery.id}`}>
+          <Header className='gr-header'>{brewery.name}</Header>
+          <div style={{color: '#504f4d'}}>
+            <Card.Description>
+              <Icon name='home' />{" "}{brewery.brewery_type}
+            </Card.Description>
+            <Card.Description>
+              <Icon name='map marker alternate' />{" "}{brewery.address}
+            </Card.Description>
+          </div>
+        </Link>
+        <Card.Description>
+          <Icon name='map marker alternate' />{" "}
+          {(brewery.website_url === 'N/A') ? 'N/A' : <a href={`${brewery.website_url}`} rel="noopener noreferrer" target='_blank'>{brewery.website_url}</a>}
+        </Card.Description>
       </Card.Content>
     </Card>
+  )
+}
+
+export function RenderPopup({brewery}) {
+
+  return (
+    <Feed>
+      <Feed.Event>
+        <Feed.Content>
+          <Header as='h3' color='blue'>{brewery.name}</Header>
+          <div style={{marginLeft: '20px'}}>
+            <Feed.Extra text><Icon name='tag' />{brewery.brewery_type}</Feed.Extra>
+            <Feed.Extra text><Icon name='map marker alternate' />{brewery.address}</Feed.Extra>
+            <Feed.Extra text><Icon name='globe' />{brewery.country}</Feed.Extra>
+            <Feed.Extra text><Icon name='barcode' />{brewery.postal_code}</Feed.Extra>
+            <Feed.Extra text><Icon name='phone' />{brewery.phone}</Feed.Extra>
+            <Feed.Extra text><Icon name='linkify' /> <a href={`${brewery.website_url}`} rel="noopener noreferrer" target='_blank'>{brewery.website_url}</a></Feed.Extra>
+          </div>
+        </Feed.Content>
+      </Feed.Event>
+    </Feed>
   )
 }
 
@@ -56,11 +81,12 @@ export function Marker({brewery}) {
     <Popup
       trigger={<Icon size='big' color='red' name='map marker alternate' />}
       content={
-        <RenderBrewery brewery={brewery} />
+        <RenderPopup brewery={brewery} />
       }
       on='click'
       wide='very'
       position='bottom center'
+      className='mapPopup'
     />
   )
 }
@@ -73,7 +99,7 @@ export function GoogleMap({brewery}) {
   const Marker = ({text}) => <Label size='tiny' color='blue' pointing>{text}</Label>;
 
   return (
-    <div style={{height: '420px', width: '100%'}}>
+    <div style={{height: '420px', width: '100%'}} id='gg-map'>
       <GoogleMapReact
         bootstrapURLKeys={{key: `${process.env.REACT_APP_GG_API}`}}
         defaultCenter={{lat: 42.165726, lng: -74.948051}} //New York State Long and Lat
