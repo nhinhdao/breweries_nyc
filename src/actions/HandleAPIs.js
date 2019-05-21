65fa6e8eeb1467406d78b1384be188f12ce835a7
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-21 19:40:03 -0400

    Added searchByName action and reducer

--- src/actions/HandleAPIs.js
+++ src/actions/HandleAPIs.js
@@ -1,31 +1,43 @@
 import axios from 'axios';
 
+// dispatch all NYC breweries to store
 export function getNYCBreweries() {
   const url = 'https://breweries-api.herokuapp.com/api/v1/places'
   return dispatch => {
     dispatch({type: "LOADING_QUERY"});
     return axios.get(url)
-      .then(resp => dispatch({type: 'GET_NYC_BREWERIES', payload: resp.data}))
+      .then(resp => dispatch({
+        type: 'GET_NYC_BREWERIES',
+        payload: resp.data
+      }))
       .catch(error => console.log(error));
   }
 }
 
+// dispatch breweries by type to store
 export function searchBreweriesByType(type) {
   const url = `https://api.openbrewerydb.org/breweries?by_state=new_york&by_type=${type}`
   return dispatch => {
     dispatch({type: "LOADING_QUERY"});
     return axios.get(url)
-      .then(resp => dispatch({type: 'SEARCH_BREWERIES_BY_TYPE', payload: resp.data}))
+      .then(resp => dispatch({
+        type: 'SEARCH_BREWERIES_BY_TYPE',
+        payload: resp.data
+      }))
       .catch(error => console.log(error));
   }
 }
 
+//dispatch brewery by name to store
 export function searchBreweriesByID(id) {
   const url = `https://api.openbrewerydb.org/breweries/${id}`
   return dispatch => {
     dispatch({type: "LOADING_QUERY"});
     return axios.get(url)
-      .then(resp => dispatch({type: 'SEARCH_BREWERIES_BY_NAME', payload: resp.data}))
+      .then(resp => dispatch({
+        type: 'SEARCH_BREWERIES_BY_NAME',
+        payload: resp.data
+      }))
       .catch(error => console.log(error));
   }
 }
--- src/reducers/nycBreweriesReducer.js
+++ src/reducers/nycBreweriesReducer.js
@@ -1,11 +1,13 @@
 export default function nycBreweriesReducer(state = defaultState, action) {
   let list, types, brewery;
   switch (action.type) {
+    //fire when the main query is loading 
     case 'LOADING_QUERY':
       return {
         ...state,
         loading: true
       }
+    //add all nyc breweries to store
     case 'GET_NYC_BREWERIES':
       types = getType(action.payload)
       return {
@@ -14,6 +16,7 @@ export default function nycBreweriesReducer(state = defaultState, action) {
         breweries: action.payload,
         loading: false
       };
+    //add breweries by type to store
     case 'SEARCH_BREWERIES_BY_TYPE':
       list = action.payload.map(place => getPlace(place))
       return {
@@ -21,6 +24,7 @@ export default function nycBreweriesReducer(state = defaultState, action) {
         breweries: list,
         loading: false
       };
+    //added brewery when user search by name to store
     case 'SEARCH_BREWERIES_BY_NAME':
       brewery = getPlace(action.payload)
       return {
@@ -28,6 +32,7 @@ export default function nycBreweriesReducer(state = defaultState, action) {
         breweryByName: brewery,
         loading: false
       };
+    //set a brewery when user click on a name from suggestion
     case 'SET_BREWERY':
       brewery = getPlace(action.payload)
       return {
@@ -35,29 +40,25 @@ export default function nycBreweriesReducer(state = defaultState, action) {
         breweryByName: brewery,
         loading: false
       };
-    case 'GET_SUGGESTION':
-      return {
-        ...state,
-        suggestion: action.payload,
-        loading: false
-      };
     default:
       return state;
   }
 }
 
+//default store with empty arrays
 const defaultState = {
   breweries: [],
-  suggestion: [],
   types: [],
   breweryByName: {},
   loading: false
 }
 
+//capitalize brewery type
 const setCap = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
+// get full address of a brewery
 const getAddress = (place) => {
   if (place.street.length > 0 && place.city.length > 0) {
     return `${place.street}, ${place.city}, New York`
@@ -70,6 +71,7 @@ const getAddress = (place) => {
   }
 }
 
+//get full details of a brewery
 const getPlace = place => {
   return {
     id: place.id,
@@ -85,6 +87,7 @@ const getPlace = place => {
   }
 }
 
+// get all types to display on the type suggestion dropdown box
 const getType = places => {
   let mixTypes = places.map(place => setCap(place.brewery_type));
   return [...new Set(mixTypes)]
